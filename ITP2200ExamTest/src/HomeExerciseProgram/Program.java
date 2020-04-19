e95a3fbe5aa0f97c280e13f3bd1b4ebec3ca80ca
saimso <56083305+saimso@users.noreply.github.com>
2020-04-19 14:47:15 +0200

    Scanner in etc.. not final

--- ITP2200ExamTest/src/HomeExerciseProgram/Program.java
+++ ITP2200ExamTest/src/HomeExerciseProgram/Program.java
@@ -46,3 +46,42 @@ public class Program{
         this.intensityLevel = intensityLevel;
     }
 }
+
+
+/*
+package homeExercisesPackage;
+import java.util.ArrayList;
+
+public class Program {
+
+    ArrayList<Exercise> lightExercises = new ArrayList<>();
+    ArrayList<Exercise> mediumExercises = new ArrayList<>();
+    ArrayList<Exercise> hardExercises = new ArrayList<>();
+
+    private String pName;
+    private int duration;
+    private int intensityLevel;
+
+    public Program(String pName, int duration, int intensityLevel) {
+        this.pName = pName;
+        this.duration = duration;
+        this.intensityLevel = intensityLevel;
+    }
+
+
+
+
+    @Override
+    public String toString() {
+
+        String programOut = "Program Name:" + " " + pName + "\n"
+        + "Program duration:" + " " + duration + "min" + "\n"
+        + "Intensity of program: " + " " + intensityLevel + "\n";
+        return programOut;
+    }
+}
+
+
+
+
+*/
