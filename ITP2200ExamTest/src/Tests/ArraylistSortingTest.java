97109c4672ef4a44b22c8aa982090a2b7e3a4b3e
j-chug <johanchg@gmail.com>
2020-04-21 15:23:20 +0200

    La til ny modifisert package med tester, nå med importerte junit biblioteker

--- ITP2200ExamTest/src/Tests/ArraylistSortingTest.java
+++ ITP2200ExamTest/src/Tests/ArraylistSortingTest.java
@@ -1,35 +1,33 @@
-/*
 package Tests;
 
-import HomeExerciseProgram.Exercise;
-import HomeExerciseProgram.Program;
-import HomeExerciseProgram.StrengthExercise;
-import org.junit.Test;
+import HomeExerciseProgram.*;
+import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 
 public class ArraylistSortingTest {
+    @Test
     public static void main(String[] args){
-        StrengthExercise s1 = new StrengthExercise(8,5,10,3,30,1,"Lift the barbell from shoulder-height and up", 33);
-        StrengthExercise s2 = new StrengthExercise(9,7,10,3,50, 12, "Lift high",77);
-        StrengthExercise s3 = new StrengthExercise(10,10,8,3,70,12, "Deadlifts", 88);
-
+        StrengthExercise se1 = new StrengthExercise("Benchpress", "Lie on bench and push barbell up", 7,5,8,3,"Barbell, bench",70);
+        EnduranceExercise ee1 = new EnduranceExercise("Intervalls", "Run for 20 seconds, then walk for 10 seconds",9,6,10,0,"Anywhere you can run");
+        FlexibilityExercise fe1 = new FlexibilityExercise("Stretch", "Do your desired stretch",1,10,0,0,"Nothing");
+        BalanceExercise be1 = new BalanceExercise("Jumprope","Jump rope",4,5,0,3,"Rope");
 
         ArrayList<Exercise> exercises = new ArrayList<>();
 
         // Adding the exercises with highest intensity first to check if they get sorted
-        exercises.add(s3);
-        exercises.add(s2);
-        exercises.add(s1);
+        exercises.add(se1);
+        exercises.add(ee1);
+        exercises.add(fe1);
+        exercises.add(be1);
 
         Program p = new Program(exercises);
 
         int intensitySortedCheck = p.getExerciseArrayList().get(0).getIntensity();
 
-        // The object with the lowest intensity should now be in the first index-spot - expected value = 8
-        assertTrue(intensitySortedCheck == 8);
+        // The object with the lowest intensity should now be in the first index-spot - expected value = 1
+        assertTrue(intensitySortedCheck == 1);
     }
 }
-*/
